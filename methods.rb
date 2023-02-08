f66352e99a63bc535de4d63c8d76fd9ba7fab26b
Enock-Yegon <ykibet70@gmail.com>
2023-02-08 14:21:55 +0300

    Completed Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+  end
+  greet_programmer
+
+#2
+  def greet(name)
+    puts "Hello, #{name}!"
+  end
+  
+  greet("Naureen")
+
+  #3
+  def greet_with_default(name ="programmer")
+    puts "Hello, #{name}!"
+  end
+  
+  greet_with_default
+
+  #4
+
+  def add(num1, num2)
+    return num1 + num2
+  end
+   puts add(1, 2)
+
+   #5
+   def halve(x)
+    if x.is_a?(Integer)
+      x / 2
+    else
+      nil
+    end
+  end
+ 
